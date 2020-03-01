283ccd79c171db759fea11b9f80f6aba866b8c51
Trieu Kiem <kntrieuit@gmail.com>
2020-03-01 12:53:38 +0700

    Changes config.

--- src/configs.js
+++ src/configs.js
@@ -1,4 +1,4 @@
 export const configObj = {
-    //serviceBaseURL: "https://siu-memo-be.herokuapp.com",
-    serviceBaseURL: "http://localhost:3030"
+    serviceBaseURL: "https://siu-memo-be.herokuapp.com",
+    // serviceBaseURL: "http://localhost:3030"
 }
